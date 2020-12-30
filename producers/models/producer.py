07aa567c610c6ba75179efe4e2b1afc961b07fe4
Kyle Stevens <kstevens715@gmail.com>
2020-12-30 15:31:05 -0500

    Step 1: Create Kafka Producers

--- producers/models/producer.py
+++ producers/models/producer.py
@@ -1,6 +1,7 @@
 """Producer base-class providing common utilites and functionality"""
 import logging
 import time
+import socket
 
 
 from confluent_kafka import avro
@@ -8,7 +9,8 @@ from confluent_kafka.admin import AdminClient, NewTopic
 from confluent_kafka.avro import AvroProducer
 
 logger = logging.getLogger(__name__)
-
+SCHEMA_REGISTRY_URL = "http://localhost:8081"
+BROKER_URL = "PLAINTEXT://localhost:9092"
 
 class Producer:
     """Defines and provides common functionality amongst Producers"""
@@ -31,36 +33,48 @@ class Producer:
         self.num_partitions = num_partitions
         self.num_replicas = num_replicas
 
-        #
-        #
-        # TODO: Configure the broker properties below. Make sure to reference the project README
-        # and use the Host URL for Kafka and Schema Registry!
-        #
-        #
-        self.broker_properties = {
-            # TODO
-            # TODO
-            # TODO
-        }
-
         # If the topic does not already exist, try to create it
         if self.topic_name not in Producer.existing_topics:
             self.create_topic()
             Producer.existing_topics.add(self.topic_name)
 
-        # TODO: Configure the AvroProducer
-        # self.producer = AvroProducer(
-        # )
+        self.producer = AvroProducer(
+            {
+                "bootstrap.servers": BROKER_URL,
+                "schema.registry.url": SCHEMA_REGISTRY_URL,
+                "client.id": socket.gethostname()
+            },
+            default_key_schema=self.key_schema,
+            default_value_schema=self.value_schema
+        )
 
     def create_topic(self):
         """Creates the producer topic if it does not already exist"""
-        #
-        #
-        # TODO: Write code that creates the topic for this producer if it does not already exist on
-        # the Kafka Broker.
-        #
-        #
-        logger.info("topic creation kafka integration incomplete - skipping")
+
+        # Had to make a client that would get garbage collected, or ran into connection problems
+        client = AdminClient({
+            "bootstrap.servers": BROKER_URL,
+            "client.id": socket.gethostname(),
+        })
+
+        if not self.topic_exists(client, self.topic_name):
+            new_topic = NewTopic(
+                topic=self.topic_name,
+                num_partitions=self.num_partitions,
+                replication_factor=self.num_replicas
+            )
+            futures = client.create_topics([new_topic])
+
+            for topic, future in futures.items():
+                try:
+                    future.result()
+                    logger.info(f"topic created {self.topic_name}")
+                except Exception as e:
+                    logger.error(f"FAILED to create topic {self.topic_name}: {e}")
+
+    def topic_exists(self, client, topic):
+        topic_response = client.list_topics()
+        return topic in set(t.topic for t in iter(topic_response.topics.values()))
 
     def time_millis(self):
         return int(round(time.time() * 1000))
