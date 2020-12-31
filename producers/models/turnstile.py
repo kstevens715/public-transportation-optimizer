e8f26bf02cf8fac7933c028e8c5147f51374638b
Kyle Stevens <kstevens715@gmail.com>
2020-12-31 11:43:46 -0500

    Step 5: Configure the KSQL Table

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -29,7 +29,7 @@ class Turnstile(Producer):
         )
 
         super().__init__(
-            f"turnstiles_{station_name}", # TODO: Come up with a better topic name
+            f"turnstiles", # TODO: Come up with a better topic name
             key_schema=Turnstile.key_schema,
             value_schema=Turnstile.value_schema,
             num_partitions=1,
