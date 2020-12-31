4a885fcbafbbc651d24399fc1490288a64b14d86
Kyle Stevens <kstevens715@gmail.com>
2020-12-31 09:29:08 -0500

    Step 2: Configure Kafka REST Proxy Producer

--- producers/models/weather.py
+++ producers/models/weather.py
@@ -30,14 +30,18 @@ class Weather(Producer):
     summer_months = set((6, 7, 8))
 
     def __init__(self, month):
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
+        # Had to move this code to the top of the initializer so the first instance
+        # would have the schemas set.
+        if Weather.key_schema is None:
+            with open(f"{Path(__file__).parents[0]}/schemas/weather_key.json") as f:
+                Weather.key_schema = json.load(f)
+
+        if Weather.value_schema is None:
+            with open(f"{Path(__file__).parents[0]}/schemas/weather_value.json") as f:
+                Weather.value_schema = json.load(f)
+
         super().__init__(
-            "weather", # TODO: Come up with a better topic name
+            "weather",
             key_schema=Weather.key_schema,
             value_schema=Weather.value_schema,
         )
@@ -48,17 +52,6 @@ class Weather(Producer):
             self.temp = 40.0
         elif month in Weather.summer_months:
             self.temp = 85.0
-
-        if Weather.key_schema is None:
-            with open(f"{Path(__file__).parents[0]}/schemas/weather_key.json") as f:
-                Weather.key_schema = json.load(f)
-
-        #
-        # TODO: Define this value schema in `schemas/weather_value.json
-        #
-        if Weather.value_schema is None:
-            with open(f"{Path(__file__).parents[0]}/schemas/weather_value.json") as f:
-                Weather.value_schema = json.load(f)
 
     def _set_weather(self, month):
         """Returns the current weather"""
@@ -73,37 +66,28 @@ class Weather(Producer):
     def run(self, month):
         self._set_weather(month)
 
-        #
-        #
-        # TODO: Complete the function by posting a weather event to REST Proxy. Make sure to
-        # specify the Avro schemas and verify that you are using the correct Content-Type header.
-        #
-        #
-        logger.info("weather kafka proxy integration incomplete - skipping")
-        #resp = requests.post(
-        #    #
-        #    #
-        #    # TODO: What URL should be POSTed to?
-        #    #
-        #    #
-        #    f"{Weather.rest_proxy_url}/TODO",
-        #    #
-        #    #
-        #    # TODO: What Headers need to bet set?
-        #    #
-        #    #
-        #    headers={"Content-Type": "TODO"},
-        #    data=json.dumps(
-        #        {
-        #            #
-        #            #
-        #            # TODO: Provide key schema, value schema, and records
-        #            #
-        #            #
-        #        }
-        #    ),
-        #)
-        #resp.raise_for_status()
+        logger.info("weather kafka proxy integration running")
+        data=json.dumps(
+            {
+                "value_schema": json.dumps(self.value_schema),
+                "key_schema": json.dumps(self.key_schema),
+                "records": [
+                    {
+                        "key": {"timestamp": self.time_millis()},
+                        "value": {
+                            "temperature": round(self.temp, 2),
+                            "status": self.status.name
+                        }
+                    }
+                ]
+            }
+        )
+        resp = requests.post(
+            f"{Weather.rest_proxy_url}/topics/{self.topic_name}",
+            headers={"Content-Type": "application/vnd.kafka.avro.v2+json"},
+            data=data
+        )
+        resp.raise_for_status()
 
         logger.debug(
             "sent weather data to kafka, temp: %s, status: %s",
